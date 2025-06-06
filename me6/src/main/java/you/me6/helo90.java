2159490db05c88283e7fbf2fe463e0e49b1a5ba1
manasviHY <test@lenovapc076>
2025-06-06 07:46:28 +0530

    initial commit

---
+++ me6/src/main/java/you/me6/helo90.java
@@ -0,0 +1,10 @@
+package you.me6;
+
+public class helo90 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ me6/src/main/java/you/me6/hiop.java
@@ -0,0 +1,10 @@
+package you.me6;
+
+public class hiop {
+
+	public static void main(String[] args) {
+		System.out.println("hello");// TODO Auto-generated method stub
+
+	}
+
+}
